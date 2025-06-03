2a2516d287833552a0635c3e77f9d1c2b74d775a
Ravan Guliyev <guliyevravan@gmail.com>
2025-06-03 16:31:27 +0400

    Portfolio içərisinə daxil olanda məlumatları göstərmə hazırlanır.

---
+++ src/_data/technologies.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: 'Vue',
+    icon: 'icon-[teenyicons--vue-solid]',
+    class: 'text-green-500',
+  },
+  {
+    name: 'NodeJS',
+    icon: 'icon-[devicon--nodejs-wordmark]',
+    class: 'text-green-500',
+  },
+  {
+    name: 'Bootstrap',
+    icon: 'icon-[devicon--bootstrap]',
+    class: 'text-purple-500',
+  },
+  {
+    name: 'Tailwind',
+    icon: 'icon-[devicon--tailwindcss]',
+    class: 'text-blue-500',
+  }
+]
