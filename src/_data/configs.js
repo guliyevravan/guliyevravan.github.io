1a9c53ea35fbd72e7705f627e1786e7e853f7581
Ravan Guliyev <guliyevravan@gmail.com>
2025-06-03 08:45:25 +0400

    Mail düzəldildi və 404 səhifəsi əlavə edildi

--- src/_data/configs.js
+++ src/_data/configs.js
@@ -1,7 +1,7 @@
 export default {
 
   title: 'Ravan Guliyev',
-  mail: 'ravan@guliyev',
+  mail: 'ravan@guliyev.dev',
   number: '+994555093993',
   whatsapp_message: `Salam. Veb sayta ehtiyacım var. Ətraflı məlumat verə bilərsinizmi?`,
 
