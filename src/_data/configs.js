99623b854063e8f63d363f14ecc31ac376ac4aff
Ravan Guliyev <ravan.guliyev@acha.local>
2025-06-25 11:15:31 +0400

    Meta url-lər düzəldildi

--- src/_data/configs.js
+++ src/_data/configs.js
@@ -1,4 +1,5 @@
 export default {
+  site_url: 'https://guliyev.dev',
   title: 'Ravan Guliyev',
   description: `Hər növ web saytların hazırlanması. Freelance web developer olaraq, 
   işinizi internetdə ən yaxşı təmsil edən, sürətli və cəzbedici saytlar hazırlayıram.`,
